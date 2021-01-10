a491c772bc9d2e02c2d495bcf5e6355b3df97a0d
thomasmburke <tburke@bu.edu>
2021-01-10 12:00:16 -0800

    updated deps and fixed breaking changes from upgrade

--- schedule_update.js
+++ schedule_update.js
@@ -7,18 +7,17 @@
  *
  * Usage:
  * node schedule_update.js [--export | --import]
- * Update schedule_export.js before importing again
+ * Update schedule_export.json before importing again
  */
 
  // TODO: Put more effort into yargs
- // TODO: Grab service account from $GOOGLE_APPLICATION_CREDENTIALS
 
-var firestoreService = require("firestore-export-import");
+const { initializeApp, restore, backup } = require('firestore-export-import')
 var fs = require('fs');
 
 // Initialize Firebase
-var serviceAccount = require("/path/to/service_account.json");
-var defaultApp = firestoreService.initializeApp(serviceAccount, "DATABASE_URL_HERE");
+var serviceAccount = require("./creds/bruinfitnessprod-firebase-adminsdk.json");
+initializeApp(serviceAccount);
 
 // Parse arguments
 var argv = require('yargs').argv;
@@ -29,15 +28,14 @@ if (argv.export && argv.import) {
   export_schedule();
 } else if (argv.import) {
   console.log('Importing BruinFitness schedule to schedule_export.js');
-  firestoreService.restore('schedule_export.json');
+  restore('schedule_export.json');
 } else {
   console.log('Unrecognized argument or no argument passed... \nPlease only pass in --export OR --import');
 }
 
 // Export your data
 function export_schedule(){
-firestoreService
-  .backup('schedules')
+backup('schedules')
   .then( function(data) {
     fs.writeFile("schedule_export.json", JSON.stringify(data, null, 4), function(err) {
         if (err) {console.log(err);}
