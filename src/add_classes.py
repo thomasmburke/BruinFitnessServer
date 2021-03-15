5408028fc356ce5bb440384d499213c1ef326dfa
thomasmburke <tburke@bu.edu>
2021-03-14 21:42:44 -0700

    added function that adds all default classes for a month

--- src/add_classes.py
+++ src/add_classes.py
@@ -660,6 +660,108 @@ DEFAULT_CLASSES = {
             'time': '7:00pm - 8:00pm',
             'workoutType': 'Open Gym'
         },
+    ],
+    'Saturday':
+    [
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '9:00am - 10:00am',
+            'workoutType': 'Weightlifting'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '8:00am - 9:00am',
+            'workoutType': 'Metcon'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '10:00am - 11:00am',
+            'workoutType': 'Metcon'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '11:15am - 12:15pm',
+            'workoutType': 'Metcon'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '8:00am - 9:00am',
+            'workoutType': 'Open Gym'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '9:00am - 10:00am',
+            'workoutType': 'Open Gym'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '10:00am - 11:00am',
+            'workoutType': 'Open Gym'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '11:00am - 12:00pm',
+            'workoutType': 'Open Gym'
+        },
+    ],
+    'Sunday':
+    [
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '9:00am - 10:00am',
+            'workoutType': 'Weightlifting'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '8:00am - 9:00am',
+            'workoutType': 'Metcon'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '10:00am - 11:00am',
+            'workoutType': 'Metcon'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '11:15am - 12:15pm',
+            'workoutType': 'Metcon'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '8:00am - 9:00am',
+            'workoutType': 'Open Gym'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '9:00am - 10:00am',
+            'workoutType': 'Open Gym'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '10:00am - 11:00am',
+            'workoutType': 'Open Gym'
+        },
+        {
+            'reservationCnt': 0,
+            'reservedUsers': [],
+            'time': '11:00am - 12:00pm',
+            'workoutType': 'Open Gym'
+        },
     ]
 }
 
@@ -686,11 +788,18 @@ def initialize_firestore_client() -> "class google.cloud.firestore_v1.client.Cli
             logger.error("Couldn't initialize firestore client via Service Account key nor GOOGLE_APPLICATION_CREDENTIALS")
             raise Exception("Couldn't initialize firestore client via Service Account key nor GOOGLE_APPLICATION_CREDENTIALS")
 
-def get_first_day_of_next_month() -> "datetime.datetime":
+def get_first_day_of_next_month(anyDatetime) -> "datetime.datetime":
     """
     Summary: Gets the first day of next month based off current datetime
     """
-    return (dt.today().replace(day=1) + timedelta(days=32)).replace(day=1)
+    return (anyDatetime.replace(day=1) + timedelta(days=32)).replace(day=1)
+
+def daterange(startDate: "datetime.datetime", endDate: "datetime.datetime"):
+    """
+    Summary: Generator function used to hide/abstract the iteration over date ranges
+    """
+    for n in range(int((endDate - startDate).days)):
+        yield startDate + timedelta(n)
 
 def add_classes():
     """
@@ -698,12 +807,25 @@ def add_classes():
     """
     #TODO: Have this run on the 20th of every month. (Cloud Scheduler -> PubSub -> GCF)?
     db = initialize_firestore_client()
-    # db.collection(f'schedules/Redwood City/dates/{today}/classes').add(DEFAULT_CLASSES[0])
 
-    classDate = get_first_day_of_next_month()
-    classDateString = classDate.strftime('%Y-%m-%d')
-    logger.info(f"classDate: {classDate.strftime('%Y-%m-%d')}")
-    logger.info(f"classDate day of week: {classDate.strftime('%A')}")
+    # Get start and end dates to populate classes for
+    # startDate: first day of next month
+    startDate = get_first_day_of_next_month(dt.today())
+    # endDate first day of the month after startDate
+    endDate = get_first_day_of_next_month(startDate)
+    logger.info(f"startDate: {startDate.strftime('%Y-%m-%d')}")
+    logger.info(f"endDate: {endDate.strftime('%Y-%m-%d')}")
+    
+    # Iterate through each date in the month
+    for classDate in daterange(startDate, endDate):
+        classDateString = classDate.strftime("%Y-%m-%d")
+        dayOfWeek = classDate.strftime('%A')
+        logger.info(f'writing class data for classDate: {classDateString}')
+        logger.info(f"classDate day of week: {dayOfWeek}")
+        # Add each class for the day separately as they all have their own reservationCnt
+        # TODO: wrap this in a try, except and send a notification in case something doesn't work as expected
+        for defaultClass in DEFAULT_CLASSES[dayOfWeek]:
+            db.collection(f'schedules/Redwood City/dates/{classDateString}/classes').add(defaultClass)
     
 
 
